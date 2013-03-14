f4d208a4a169bf16013837dad261a91b6172fb27
itsbenweeks <bweeks@gmail.com>
2013-03-14 07:35:58 -0400

    Finally settled on a solution for the newton problem I am happy with

--- Python/CIT523/PSet3/ps3_newton.py
+++ Python/CIT523/PSet3/ps3_newton.py
@@ -42,7 +42,6 @@ def computeDeriv(poly):
     return result
 
 # Problem 3: Newton's Method
-count = 0
 def computeRoot(poly, x_0, epsilon):
     '''
     Uses Newton's method to find and return a root of a polynomial function.
@@ -56,15 +55,15 @@ def computeRoot(poly, x_0, epsilon):
     epsilon: float > 0
     returns: list [float, int]
     '''
-    
-    x_0
     x_1 = x_0-(evaluatePoly(poly, x_0)/evaluatePoly(computeDeriv(poly),x_0))
-    count = 0
-    if abs(x_1-x_0)> epsilon:
+    product=[x_1, 0]
+    if abs(product[0]-x_0)> epsilon:
         #print x_1, count
-        return computeRoot(poly, x_1, epsilon), count+1
-    
-    return [x_0, count+1]
+        product=computeRoot(poly, x_1, epsilon)
+        product[1]=product[1]+1
+        return product
+    product =[x_0, 0]
+    return product
 
 # Test Cases
 def test(function):
@@ -72,3 +71,9 @@ def test(function):
         print computeRoot([-13.39, 0.0, 17.5, 3.0, 1.0], 0.1, .0001)==[0.806790753796352, 7]
         print computeRoot([1, 9, 8], -3, .01)==[-1.0000079170005467, 5]
         print computeRoot([1, -1, 1, -1], 2, .001)==[1.0002210630197605, 4]
+        print computeRoot([1, 2, 3, 4.3, -5], 0.3, 0.0001)==[-0.47771869826311336, 6]
+        print computeRoot([-8, 2, 1], 20, 0.0001)==[2.0000000605441395, 6]
+        print computeRoot([-8, 2, 1], 2, 0.0001)==[2, 0]
+        print computeRoot([-8, 2, 1], -20, 0.0001)==[-4.00000000844848, 6]
+        print computeRoot([-8, 2, 1], -4, 0.0001) ==[-4, 0]
+        print computeRoot([4, 56, 0, 28, 0, 14, 0, 1], 1, 0.0001)==[-0.07124736072967754, 4]
