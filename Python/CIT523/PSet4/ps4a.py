a1b0eb938d7eb2da405269a99c18754e7a874da4
itsbenweeks <bweeks@gmail.com>
2013-04-01 12:53:13 -0400

    Supposedly solved Problem #5

--- Python/CIT523/PSet4/ps4a.py
+++ Python/CIT523/PSet4/ps4a.py
@@ -257,22 +257,31 @@ def playHand(hand, wordList, n):
 # 
 
 def playGame(wordList):
-    """
+	"""
     Allow the user to play an arbitrary number of hands.
 
-    1) Asks the user to input 'n' or 'r' or 'e'.
-      * If the user inputs 'n', let the user play a new (random) hand.
-      * If the user inputs 'r', let the user play the last hand again.
-      * If the user inputs 'e', exit the game.
-      * If the user inputs anything else, tell them their input was invalid.
+	1) Asks the user to input 'n' or 'r' or 'e'.
+	  * If the user inputs 'n', let the user play a new (random) hand.
+	  * If the user inputs 'r', let the user play the last hand again.
+	  * If the user inputs 'e', exit the game.
+	  * If the user inputs anything else, tell them their input was invalid.
  
-    2) When done playing the hand, repeat from step 1    
-    """
-    # TO DO ... <-- Remove this comment when you code this function
-    print "playGame not yet implemented." # <-- Remove this line when you code the function
-   
-
-
+	2) When done playing the hand, repeat from step 1    
+	"""
+	choice =''
+	hand = {}
+	while not choice.startswith('e'):
+		choice = raw_input('Enter n to deal a new hand, r to replay the last hand, or e to end game: ').lower()
+		if choice.startswith('n'):
+			hand=dealHand(HAND_SIZE)
+			playHand(hand, wordList, HAND_SIZE)
+		elif choice.startswith('r'):
+			if calculateHandlen(hand)==0:
+				print 'You have not played a hand yet. Please play a new hand first!'
+			else: 
+				playHand(hand, wordList, HAND_SIZE)
+		elif not choice.startswith('e'):
+			print 'Invalid command.'
 
 #
 # Build data structures used for entire session and play game
