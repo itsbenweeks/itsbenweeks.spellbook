b1db4101611edd046718c12508870626ea4f61d3
Ben Weeks <bweeks@gmail.com>
2016-08-06 14:18:06 -0400

    Now with sequences and triggers.

--- database_architecture/retail_mgmg_system/CREATE_TABLES.sql
+++ database_architecture/retail_mgmg_system/CREATE_TABLES.sql
@@ -4,6 +4,15 @@ CREATE TABLE "EMPLOYEE" (
   "LastName" varchar2(25),
   "SSN" varchar2(11)
 );
+create sequence employee_id_seq start with 1 increment by 1;
+create trigger trg_employee_id
+  before insert on "EmployeeID"
+  for each row
+    begin
+          select employee_id_seq.nextval
+          into :new."EmployeeID"
+          from dual;
+    end;
 
 CREATE TABLE "CUSTOMER" (
   "CustomerID" number primary key,
@@ -14,6 +23,15 @@ CREATE TABLE "CUSTOMER" (
   "ZipCode" varchar2(14),
   "TelephoneNumber" varchar2(17)
 );
+CREATE sequence customer_id_seq start with 1 increment by 1;
+CREATE trigger trg_customer_id
+  before insert on "CustomerID"
+  for each row
+    begin
+          select customer_id_seq.nextval
+          into :new."CustomerID"
+          from dual;
+    end;
 
 CREATE TABLE "VENDOR" (
   "VendorID" number primary key,
@@ -24,6 +42,15 @@ CREATE TABLE "VENDOR" (
   "ZipCode" varchar2(14),
   "TelephoneNumber" varchar2(17)
 );
+CREATE sequence vendor_id_seq start with 1 increment by 1;
+CREATE trigger trg_vendor_id
+  before insert on VendorID
+  for each row
+    begin
+          select vendor_id_seq.nextval
+          into :new."VendorID"
+          from dual;
+    end;
 
 CREATE TABLE "ITEM" (
   "ItemSKU" varchar2(11) primary key,
@@ -47,6 +74,15 @@ CREATE TABLE "PURCHASE" (
   "PurchaseReceive" date,
   "Available" number(3)
 );
+CREATE sequence purchase_id_seq start with 1 increment by 1;
+CREATE trigger trg_purchase_id
+  before insert on "PurchaseID"
+  for each row
+    begin
+          select purchase_id_seq.nextval
+          into :new."PurchaseID":
+          from dual;
+    end;
 
 CREATE TABLE "ORDER" (
   "OrderID" number primary key,
@@ -56,3 +92,12 @@ CREATE TABLE "ORDER" (
   "CustomerID" number references CUSTOMER("CustomerID"),
   "EmployeeID" number references EMPLOYEE("EmployeeID")
 );
+CREATE sequence order_id_seq start with 1 increment by 1;
+CREATE trigger trg_order_id
+  before insert on "OrderID"
+  for each row
+    begin
+          select order_id_seq.nextval
+          into :new."OrderID"
+          from dual;
+    end;
