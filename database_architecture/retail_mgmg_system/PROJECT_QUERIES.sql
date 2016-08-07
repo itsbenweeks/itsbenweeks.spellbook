cef9b3b999ca65e96189d8e994deaf90515cd324
Ben Weeks <bweeks@gmail.com>
2016-08-07 15:16:25 -0400

    A stored procedure

--- database_architecture/retail_mgmg_system/PROJECT_QUERIES.sql
+++ database_architecture/retail_mgmg_system/PROJECT_QUERIES.sql
@@ -1,30 +1,30 @@
 --- Search for Customer Info from Customer Name
-Select * 
+Select *
 from CUSTOMER
 Where "CustomerID" = &ID;
 
 
 -- Search for Customer Number from Customer First Name and Last Name
-Select "CustomerID" 
+Select "CustomerID"
 from Customer
 Where "FirstName" = '&FN' and "LastName" = '&LN';
 
 
 -- Search for Available Quantity in Inventory for an Item--
-Select "ItemName","Quantity" 
+Select "ItemName","Quantity"
 from INVENTORY, ITEM
 Where INVENTORY."ItemSKU" = ITEM."ItemSKU" and INVENTORY."ItemSKU" = '&SK';
 
 
 -- Search for Item Name using SKU--
 
-Select "ItemName","Size" 
+Select "ItemName","Size"
 from  ITEM
 Where ITEM."ItemSKU" = '&SKU';
 
 
 -- Update Inventory quantities for an Item--
-UPDATE INVENTORY 
+UPDATE INVENTORY
 SET "Quantity" = '&Quantity'
 WHERE INVENTORY."ItemSKU" = '&SKU';
 
@@ -40,8 +40,22 @@ select INVENTORY."ItemSKU", sum(INVENTORY."Quantity") as "Quantity"
 from INVENTORY 
 group by INVENTORY."ItemSKU";
 
--- Query to get most ordered items in the past year--
-
-SELECT "ITEM"."ItemName" as "Name", "ORDER"."ItemSKU" as "SKU", SUM("ORDER"."Quantity") AS "Quantity"
-FROM "ORDER" INNER JOIN "ITEM" ON "ITEM"."ItemSKU" = "ORDER"."ItemSKU" WHERE "ORDER"."Date" > SYSDATE - 365 -- May want to worry about leap years
-GROUP BY "ORDER"."ItemSKU", "ITEM"."ItemName" ORDER BY "Quantity" DESC;
+-- Procedure to get most ordered items in the past year--
+CREATE OR REPLACE PROCEDURE top_items AS
+SET SERVEROUTPUT ON;
+DECLARE
+  CURSOR top_item IS
+    SELECT "ITEM"."ItemName" AS "Name", "ORDER"."ItemSKU" AS "SKU", SUM("ORDER"."Quantity") AS "Quantity"
+      FROM "ORDER" INNER JOIN "ITEM" ON "ITEM"."ItemSKU" = "ORDER"."ItemSKU" WHERE "ORDER"."Date" > SYSDATE - 365
+      GROUP BY "ORDER"."ItemSKU", "ITEM"."ItemName" ORDER BY "Quantity" DESC;
+  top_item_row top_item%ROWTYPE;
+BEGIN
+  OPEN top_item;
+  LOOP
+    FETCH top_item INTO top_item_row;
+    EXIT WHEN top_item%NOTFOUND;
+    DBMS_OUTPUT.PUT_LINE('The item "'||top_item_row."Name"||
+      '" has been purchased '||top_item_row."Quantity" ||' times.');
+  END LOOP;
+  CLOSE top_item;
+END;
